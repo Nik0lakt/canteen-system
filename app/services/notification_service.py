ce15a5f80391b9a5f1dcd62ce76fad9cb1290125
Nik0lakt <149397596+Nik0lakt@users.noreply.github.com>
2025-12-11 18:47:01 +0300

    Create notification_service.py

---
+++ app/services/notification_service.py
@@ -0,0 +1,12 @@
+def send_payment_notification(
+    telegram_id: str,
+    amount_cents: int,
+    subsidy_cents: int,
+    salary_cents: int,
+    subsidy_remaining_cents: int,
+    monthly_remaining_cents: int,
+):
+    """
+    Заглушка. Здесь можно реализовать отправку уведомления в Telegram.
+    """
+    pass
